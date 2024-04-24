bc96c19b93d3be4020c0b8e33ec3084a16f1e654
Ahlecss <alex.sejourne1@gmail.com>
2024-04-25 00:48:31 +0200

    feat(is that the whole project in one day ?)

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
 import * as THREE from 'three'
-import { useEffect, useRef, useState } from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
-import { useCursor, MeshReflectorMaterial, Image, Text, Environment } from '@react-three/drei'
+import { useEffect, useRef, useState, forwardRef } from 'react'
+import { Canvas, useFrame, useThree } from '@react-three/fiber'
+import { useCursor, MeshReflectorMaterial, Image, Text, Environment, Html, useTexture, useScroll, ScrollControls, Scroll, SpotLight, useDepthBuffer } from '@react-three/drei'
 import { useRoute, useLocation } from 'wouter'
 import { easing } from 'maath'
 import getUuid from 'uuid-by-string'
+import tunnel from 'tunnel-rat'
+import useSound from 'use-sound';
+
+import { lerp } from './utils.js'
+import { EffectComposer, GodRays, Bloom } from '@react-three/postprocessing'
 
 const GOLDENRATIO = 1.61803398875
+const ui = tunnel()
 
-export const App = ({ images }) => (
-  <Canvas dpr={[1, 1.5]} camera={{ fov: 70, position: [0, 2, 15] }}>
-    <color attach="background" args={['#191920']} />
-    <fog attach="fog" args={['#191920', 0, 15]} />
-    <group position={[0, -0.5, 0]}>
-      <Frames images={images} />
-      <mesh rotation={[-Math.PI / 2, 0, 0]}>
+
+export const App = ({ vitraux }) => {
+  return (
+    <>
+      <div id='dom-portal'>
+        <ui.Out />
+
+      </div>
+      <Canvas shadows dpr={[1, 1.5]} camera={{ fov: 70, position: [0, 2, 15] }}>
+        <ambientLight intensity={0.015} />
+        <color attach="background" args={['#191920']} />
+        <fog attach="fog" args={['#191920', 0, 15 * 2]} />
+        <group position={[0, -0.5, 0]}>
+          <ScrollControls pages={1.45} damping={0.1}>
+            <ScrollWrapper vitraux={vitraux} />
+          </ScrollControls>
+          {/* <mesh rotation={[-Math.PI / 2, 0, 0]}>
         <planeGeometry args={[50, 50]} />
         <MeshReflectorMaterial
           blur={[300, 100]}
@@ -28,75 +44,161 @@ export const App = ({ images }) => (
           color="#050505"
           metalness={0.5}
         />
-      </mesh>
-    </group>
-    <Environment preset="city" />
-  </Canvas>
-)
+      </mesh> */}
+        </group>
+        <Environment preset="city" />
+        <mesh receiveShadow position={[0, -10, 0]} rotation-x={-Math.PI / 2}>
+          <planeGeometry args={[50, 50]} />
+          <meshPhongMaterial />
+        </mesh>
+      </Canvas>
+    </>
+  )
+}
 
-function Frames({ images, q = new THREE.Quaternion(), p = new THREE.Vector3() }) {
-  const ref = useRef()
+function ScrollWrapper({ vitraux }) {
+  return (
+    <>
+      <Frames vitraux={vitraux} />
+    </>
+  )
+}
+
+function Frames({ vitraux, q = new THREE.Quaternion(), p = new THREE.Vector3() }) {
+  const ref = useRef([])
   const clicked = useRef()
+  const itemsRef = useRef([]);
   const [, params] = useRoute('/item/:id')
   const [, setLocation] = useLocation()
+  const data = useScroll()
+  let active = false;
+
   useEffect(() => {
-    clicked.current = ref.current.getObjectByName(params?.id)
+    const a = data.range(0, 1)
+    // clicked.current = ref.current.getObjectByName(params?.id)
     if (clicked.current) {
       clicked.current.parent.updateWorldMatrix(true, true)
-      clicked.current.parent.localToWorld(p.set(0, GOLDENRATIO / 2, 1.25))
+      clicked.current.parent.localToWorld(p.set(1.5, 5, 3.25))
       clicked.current.parent.getWorldQuaternion(q)
+      active = true;
     } else {
       p.set(0, 0, 5.5)
       q.identity()
+      active = false;
     }
+
   })
   useFrame((state, dt) => {
+    if (!active && data) data.el.scrollTop = lerp(data.el.scrollTop, 0, 0.1)
     easing.damp3(state.camera.position, p, 0.4, dt)
     easing.dampQ(state.camera.quaternion, q, 0.4, dt)
   })
   return (
-    <group
-      ref={ref}
-      onClick={(e) => (e.stopPropagation(), setLocation(clicked.current === e.object ? '/' : '/item/' + e.object.name))}
-      onPointerMissed={() => setLocation('/')}>
-      {images.map((props) => <Frame key={props.url} {...props} /> /* prettier-ignore */)}
-    </group>
+    <>
+      <group
+        ref={ref}
+        onClick={(e) => (e.stopPropagation(), setLocation(clicked.current === e.object ? '/' : '/item/' + e.object.name))}
+        onPointerMissed={() => setLocation('/')}>
+        {vitraux.map((props, i) => <Frame key={props.url} i={i} {...props} ref={itemsRef} /> /* prettier-ignore */)}
+      </group>
+      {itemsRef.current[0] && (
+        <EffectComposer disableNormalPass multisampling={0}>
+          <GodRays sun={itemsRef.current[0]} exposure={0.14} decay={0.8} blur />
+          <Bloom luminanceThreshold={0} mipmapBlur luminanceSmoothing={0.0} intensity={1} />
+        </EffectComposer>
+      )}
+    </>
+
   )
 }
 
-function Frame({ url, c = new THREE.Color(), ...props }) {
-  const image = useRef()
-  const frame = useRef()
+const Frame = forwardRef((props, itemsRef) => {
+  const { url, music, texture, i, idd } = props
+  const vitrail = useRef()
+  // const frame = useRef()
+  const vec = new THREE.Vector3()
+  const c = new THREE.Color()
   const [, params] = useRoute('/item/:id')
   const [hovered, hover] = useState(false)
+  const [invisible, setInvisible] = useState(false)
+  const [isPlaying, setIsPlaying] = useState(false);
   const [rnd] = useState(() => Math.random())
   const name = getUuid(url)
   const isActive = params?.id === name
+  const [play, { stop, sound }] = useSound(music, {});
+  const light = useRef()
+
+  const viewport = useThree((state) => state.viewport)
+
+
+  const depthBuffer = useDepthBuffer();
+
+  const tex = useTexture(texture)
+  const handleHover = (s, h) => {
+    hover(h)
+    if (!isPlaying && (h || isActive)) {
+      setIsPlaying(true)
+      s.fade(0, 1, 1000)
+      play()
+    } else if ((!h && !isActive)) {
+      sound.fade(1, 0, 250)
+      setIsPlaying(false)
+      setTimeout(() => { stop() }, 250)
+    }
+  }
+
   useCursor(hovered)
   useFrame((state, dt) => {
-    image.current.material.zoom = 2 + Math.sin(rnd * 10000 + state.clock.elapsedTime / 3) / 2
-    easing.damp3(image.current.scale, [0.85 * (!isActive && hovered ? 0.85 : 1), 0.9 * (!isActive && hovered ? 0.905 : 1), 1], 0.1, dt)
-    easing.dampC(frame.current.material.color, hovered ? 'orange' : 'white', 0.1, dt)
+
+    // console.log(itemsRef.current[i])
+    console.log(itemsRef.current[i])
+
+    // light.current.target.position.lerp(vec.set((state.mouse.x * viewport.width) / 2, (state.mouse.y * viewport.height) / 2, 0), 0.1)
+    // light.current.target.updateMatrixWorld()
+    if (idd % 2 === 0) {
+      // vitrail.current.needsUpdate = true
+      easing.damp(itemsRef.current[i].material, 'opacity', invisible ? 0 : 1, 0.1, dt)
+    }
+    // vitrail.current.material.zoom = 2 + Math.sin(rnd * 10000 + state.clock.elapsedTime / 3) / 2
+
+    // if (props.idd % 2 !== 0) {
+    easing.damp3(itemsRef.current[i].scale, (!isActive && hovered ? 1.1 : 1), 0.1, dt)
+    // }
+    // easing.dampC(frame.current.material.color, hovered ? '#D31D1C' : 'white', 0.1, dt)
   })
   return (
     <group {...props}>
-      <mesh
-        name={name}
-        onPointerOver={(e) => (e.stopPropagation(), hover(true))}
-        onPointerOut={() => hover(false)}
-        scale={[1, GOLDENRATIO, 0.05]}
-        position={[0, GOLDENRATIO / 2, 0]}>
-        <boxGeometry />
-        <meshStandardMaterial color="#151515" metalness={0.5} roughness={0.5} envMapIntensity={2} />
-        <mesh ref={frame} raycast={() => null} scale={[0.9, 0.93, 0.9]} position={[0, 0, 0.2]}>
+      {/* <mesh ref={frame} raycast={() => null} scale={[0.9, 0.93, 0.9]} position={[0, 0, 0.2]}>
           <boxGeometry />
           <meshBasicMaterial toneMapped={false} fog={false} />
-        </mesh>
-        <Image raycast={() => null} ref={image} position={[0, 0, 0.7]} url={url} />
-      </mesh>
-      <Text maxWidth={0.1} anchorX="left" anchorY="top" position={[0.55, GOLDENRATIO, 0]} fontSize={0.025}>
+        </mesh> */}
+      <Scroll>
+
+        <group ref={vitrail} >
+          <mesh receiveShadow position={[0, 0, 0.1]}
+            ref={el => itemsRef.current[i] = el}
+            key={i}
+            name={name}
+            transparent={true}
+            onPointerOver={(e) => (e.stopPropagation(), handleHover(sound, true))}
+            onPointerOut={() => (handleHover(sound, false))}>
+            <planeBufferGeometry attach="geometry" args={[3.1 * 1.3, 10 * 1.3]} />
+            <meshBasicMaterial attach="material" map={tex} transparent={true} colorSpace={THREE.SRGBColorSpace} />
+          </mesh>
+        </group>
+
+        {/* <SpotLight castShadow ref={light} penumbra={0} distance={60} angle={3.5} attenuation={5} anglePower={4} intensity={2} color="red" position={[0, 5, 1.5]} depthBuffer={depthBuffer} /> */}
+      </Scroll>
+      <ui.In>
+        <div className={`cartel_wrapper ${isActive ? 'cartel_active' : ''}`}>
+          <h1 className='cartel_title'>{props.genre}</h1>
+          <p className='cartel_description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <buttton className='cartel_button' onClick={(e) => (e.stopPropagation(), setInvisible(!invisible), handleHover(sound, true))}>Discover references</buttton>
+        </div>
+      </ui.In>
+      {/* <Text maxWidth={0.1} anchorX="left" anchorY="bottom" position={[0.55, GOLDENRATIO, 0]} fontSize={0.025}>
         {name.split('-').join(' ')}
-      </Text>
+      </Text> */}
     </group>
   )
-}
+})
--- src/index.js
+++ src/index.js
@@ -2,21 +2,31 @@ import { createRoot } from 'react-dom/client'
 import './styles.css'
 import { App } from './App'
 
-const pexel = (id) => `https://images.pexels.com/photos/${id}/pexels-photo-${id}.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260`
-const images = [
+import black from './images/VitrailSimple_BLACKMETAL.png'
+import hardcore from './images/VitrailSimple_HARDCORE.png'
+import death from './images/VitrailSimple_DEATHMETAL.png'
+
+import blackLegendes from './images/VitrailSimple_BLACKMETAL_Legendes.jpg'
+import hardcoreLegendes from './images/VitrailSimple_HARDCORE_Legendes.jpg'
+import deathLegendes from './images/VitrailSimple_DEATHMETAL_Legendes.jpg'
+
+import SpiritCrusher from './sounds/SpiritCrusher.mp3'
+import HornetsNest from './sounds/HornetsNest.mp3'
+import FreezingMoon from './sounds/FreezingMoon.mp3'
+
+const pexel = (id) => `${id}`
+// const pexel = (id) => `https://images.pexels.com/photos/${id}/pexels-photo-${id}.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260`
+
+const vitraux = [
   // Front
-  { position: [0, 0, 1.5], rotation: [0, 0, 0], url: pexel(1103970) },
-  // Back
-  { position: [-0.8, 0, -0.6], rotation: [0, 0, 0], url: pexel(416430) },
-  { position: [0.8, 0, -0.6], rotation: [0, 0, 0], url: pexel(310452) },
+  { position: [0, 0, 1.5 * -4], rotation: [0, 0, 0], url: '0', texture: black, idd: 0, genre: 'Black Metal', music: FreezingMoon },
+  { position: [0, 0, 1.51 * -4], rotation: [0, 0, 0], url: '1', texture: blackLegendes, idd: 1 },
   // Left
-  { position: [-1.75, 0, 0.25], rotation: [0, Math.PI / 2.5, 0], url: pexel(327482) },
-  { position: [-2.15, 0, 1.5], rotation: [0, Math.PI / 2.5, 0], url: pexel(325185) },
-  { position: [-2, 0, 2.75], rotation: [0, Math.PI / 2.5, 0], url: pexel(358574) },
+  { position: [-9, 0, -3.75], rotation: [0, Math.PI / 2.5, 0], url: '2', texture: hardcore, idd: 2, genre: 'Harcore', music: HornetsNest },
+  { position: [-9.01, 0.01, -3.76], rotation: [0, Math.PI / 2.5, 0], url: '3', texture: hardcoreLegendes, idd: 3 },
   // Right
-  { position: [1.75, 0, 0.25], rotation: [0, -Math.PI / 2.5, 0], url: pexel(227675) },
-  { position: [2.15, 0, 1.5], rotation: [0, -Math.PI / 2.5, 0], url: pexel(911738) },
-  { position: [2, 0, 2.75], rotation: [0, -Math.PI / 2.5, 0], url: pexel(1738986) }
+  { position: [9, 0, -3.75], rotation: [0, -Math.PI / 2.5, 0], url: '4', texture: death, idd: 4, genre: 'Death Metal', music: SpiritCrusher },
+  { position: [9.01, 0.01, -3.76], rotation: [0, -Math.PI / 2.5, 0], url: '5', texture: deathLegendes, idd: 5 }
 ]
 
-createRoot(document.getElementById('root')).render(<App images={images} />)
+createRoot(document.getElementById('root')).render(<App vitraux={vitraux} />)
