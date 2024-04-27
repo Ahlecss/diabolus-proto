0e732d624cc57c19a5c7b2e0c572feacf8baed85
Ahlecss <alex.sejourne1@gmail.com>
2024-04-27 17:08:07 +0200

    feat(noise on vertex)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three'
 import { useEffect, useRef, useState, forwardRef, useCallback, Suspense, memo } from 'react'
-import { Canvas, useFrame, useThree,extend } from '@react-three/fiber'
+import { Canvas, useFrame, useThree, extend } from '@react-three/fiber'
 import { useCursor, MeshReflectorMaterial, Image, Text, Environment, Html, useTexture, useScroll, ScrollControls, Scroll, SpotLight, useDepthBuffer, shaderMaterial } from '@react-three/drei'
 import { useRoute, useLocation } from 'wouter'
 import { easing } from 'maath'
@@ -11,6 +11,7 @@ import useSound from 'use-sound';
 
 import { lerp } from './utils.js'
 import { EffectComposer, GodRays, Bloom } from '@react-three/postprocessing'
+import { log } from 'three/examples/jsm/nodes/Nodes.js'
 
 const GOLDENRATIO = 1.61803398875
 const ui = tunnel()
@@ -87,8 +88,9 @@ function Frames({ vitraux, q = new THREE.Quaternion(), p = new THREE.Vector3() }
   const [, setLocation] = useLocation()
   const data = useScroll()
   let active = false;
-  let exposure = 0;
+  const [expo, setExpo] = useState(0);
   useEffect(() => {
+    console.log('useEffect')
     const a = data.range(0, 1)
     clicked.current = ref.current.getObjectByName(params?.id)
     if (clicked.current) {
@@ -110,6 +112,13 @@ function Frames({ vitraux, q = new THREE.Quaternion(), p = new THREE.Vector3() }
   const handleGodrays = useCallback(change, [setHovered])
   useFrame((state, dt) => {
     if (!active && data) data.el.scrollTop = lerp(data.el.scrollTop, 0, 0.1)
+
+    if (god.current && god.current.blendMode) {
+      god.current.blendMode.opacity.value = lerp(god.current.blendMode.opacity.value, Math.abs(Math.sin(state.clock.elapsedTime)), cubic.inOut(0.7))
+      console.log(god.current.blendMode.opacity.value)
+      // if (hovered) easing.damp(god.current.blendMode.opacity, 'value', 1, cubic.inOut(0.6), dt)
+      // else easing.damp(god.current.blendMode.opacity, 'value', 0, cubic.inOut(0.6), dt)
+    }
     // Try to change exposure
     // if (hovered != 1) exposure = lerp(exposure, 0.2, 0.1)
     // else exposure = lerp(exposure, 0, 0.1)
@@ -126,7 +135,7 @@ function Frames({ vitraux, q = new THREE.Quaternion(), p = new THREE.Vector3() }
       </group>
       {itemsRef.current[0] && (
         <EffectComposer disableNormalPass multisampling={0}>
-          {hovered < 6 && <GodRays ref={god} sun={itemsRef.current[hovered]} exposure={0.1} decay={0.8} blur />}
+          {hovered < 6 && <GodRays ref={god} sun={itemsRef.current[hovered]} density={0.4} weight={0.4} exposure={0.4} decay={0.8} blur />}
           <Bloom luminanceThreshold={0} mipmapBlur luminanceSmoothing={0.0} intensity={0.3} />
         </EffectComposer>
       )}
@@ -139,12 +148,126 @@ export const ImageFadeMaterial = shaderMaterial(
   {
     tex1: undefined,
     tex2: undefined,
-    opac: 0
+    opac: 0,
+    time: 0
   },
   ` varying vec2 vUv;
+
+  uniform float time;
+
+  //
+  // Description : Array and textureless GLSL 2D/3D/4D simplex
+  //               noise functions.
+  //      Author : Ian McEwan, Ashima Arts.
+  //  Maintainer : ijm
+  //     Lastmod : 20110822 (ijm)
+  //     License : Copyright (C) 2011 Ashima Arts. All rights reserved.
+  //               Distributed under the MIT License. See LICENSE file.
+  //               https://github.com/ashima/webgl-noise
+  //
+  
+  vec3 mod289(vec3 x) {
+    return x - floor(x * (1.0 / 289.0)) * 289.0;
+  }
+  
+  vec4 mod289(vec4 x) {
+    return x - floor(x * (1.0 / 289.0)) * 289.0;
+  }
+  
+  vec4 permute(vec4 x) {
+       return mod289(((x*34.0)+1.0)*x);
+  }
+  
+  vec4 taylorInvSqrt(vec4 r)
+  {
+    return 1.79284291400159 - 0.85373472095314 * r;
+  }
+  
+  float snoise(vec3 v) {
+    const vec2  C = vec2(1.0/6.0, 1.0/3.0) ;
+    const vec4  D = vec4(0.0, 0.5, 1.0, 2.0);
+    
+    // First corner
+    vec3 i  = floor(v + dot(v, C.yyy) );
+    vec3 x0 =   v - i + dot(i, C.xxx) ;
+    
+    // Other corners
+    vec3 g = step(x0.yzx, x0.xyz);
+    vec3 l = 1.0 - g;
+    vec3 i1 = min( g.xyz, l.zxy );
+    vec3 i2 = max( g.xyz, l.zxy );
+  
+    //   x0 = x0 - 0.0 + 0.0 * C.xxx;
+    //   x1 = x0 - i1  + 1.0 * C.xxx;
+    //   x2 = x0 - i2  + 2.0 * C.xxx;
+    //   x3 = x0 - 1.0 + 3.0 * C.xxx;
+    vec3 x1 = x0 - i1 + C.xxx;
+    vec3 x2 = x0 - i2 + C.yyy; // 2.0*C.x = 1/3 = C.y
+    vec3 x3 = x0 - D.yyy;      // -1.0+3.0*C.x = -0.5 = -D.y
+    
+    // Permutations
+    i = mod289(i);
+    vec4 p = permute( permute( permute(
+               i.z + vec4(0.0, i1.z, i2.z, 1.0 ))
+             + i.y + vec4(0.0, i1.y, i2.y, 1.0 ))
+             + i.x + vec4(0.0, i1.x, i2.x, 1.0 ));
+             
+    // Gradients: 7x7 points over a square, mapped onto an octahedron.
+    // The ring size 17*17 = 289 is close to a multiple of 49 (49*6 = 294)
+    float n_ = 0.142857142857; // 1.0/7.0
+    vec3  ns = n_ * D.wyz - D.xzx;
+  
+    vec4 j = p - 49.0 * floor(p * ns.z * ns.z);  //  mod(p,7*7)
+  
+    vec4 x_ = floor(j * ns.z);
+    vec4 y_ = floor(j - 7.0 * x_ );    // mod(j,N)
+  
+    vec4 x = x_ *ns.x + ns.yyyy;
+    vec4 y = y_ *ns.x + ns.yyyy;
+    vec4 h = 1.0 - abs(x) - abs(y);
+  
+    vec4 b0 = vec4( x.xy, y.xy );
+    vec4 b1 = vec4( x.zw, y.zw );
+  
+    //vec4 s0 = vec4(lessThan(b0,0.0))*2.0 - 1.0;
+    //vec4 s1 = vec4(lessThan(b1,0.0))*2.0 - 1.0;
+    vec4 s0 = floor(b0)*2.0 + 1.0;
+    vec4 s1 = floor(b1)*2.0 + 1.0;
+    vec4 sh = -step(h, vec4(0.0));
+  
+    vec4 a0 = b0.xzyw + s0.xzyw*sh.xxyy ;
+    vec4 a1 = b1.xzyw + s1.xzyw*sh.zzww ;
+  
+    vec3 p0 = vec3(a0.xy,h.x);
+    vec3 p1 = vec3(a0.zw,h.y);
+    vec3 p2 = vec3(a1.xy,h.z);
+    vec3 p3 = vec3(a1.zw,h.w);
+    
+    // Normalise gradients
+    vec4 norm = taylorInvSqrt(vec4(dot(p0,p0), dot(p1,p1), dot(p2, p2), dot(p3,p3)));
+    p0 *= norm.x;
+    p1 *= norm.y;
+    p2 *= norm.z;
+    p3 *= norm.w;
+    
+    // Mix final noise value
+    vec4 m = max(0.6 - vec4(dot(x0,x0), dot(x1,x1), dot(x2,x2), dot(x3,x3)), 0.0);
+    m = m * m;
+    return 42.0 * dot( m*m, vec4( dot(p0,x0), dot(p1,x1),
+                                  dot(p2,x2), dot(p3,x3) ) );
+  }
+
     void main() {
       vUv = uv;
-      gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+
+      vec3 pos = position;
+
+      float noiseFreq = 0.5;
+      float noiseAmp = 0.25; 
+      vec3 noisePos = vec3(pos.x * noiseFreq + time * 0.01, pos.y * noiseFreq + time * 0.004, pos.z);
+      pos.z += snoise(noisePos) * noiseAmp;
+
+      gl_Position = projectionMatrix * modelViewMatrix * vec4( pos, 1.0 );
     }`,
   ` varying vec2 vUv;
     uniform sampler2D tex1;
@@ -202,10 +325,14 @@ const Frame = memo(forwardRef((props, itemsRef) => {
 
   console.log('rerender')
 
+  console.log(shaderRef.current)
+
   useCursor(hovered.current)
   useFrame((state, dt) => {
     // console.log(idd, invisible.current)
     // console.log(vitrail.current.children[0])
+  shaderRef.current.uniforms.time.value += 1
+  console;log(shaderRef.current)
     easing.damp(shaderRef.current, 'opac', invisible.current ? 1 : 0, cubic.inOut(0.4), dt)
     easing.damp3(vitrail.current.children[0].scale, (!isActive && hovered.current ? 1.1 : 1), cubic.inOut(0.4), dt)
   })
@@ -226,7 +353,7 @@ const Frame = memo(forwardRef((props, itemsRef) => {
             transparent={true}
             onPointerOver={(e) => (e.stopPropagation(), handleHover(sound, true))}
             onPointerOut={() => (handleHover(sound, false))}>
-            <planeBufferGeometry attach="geometry" args={[3.1 * 1.3, 10 * 1.3]} />
+            <planeBufferGeometry attach="geometry" args={[3.1 * 1.3, 10 * 1.3, 100, 100]} />
             <imageFadeMaterial ref={shaderRef} attach="material" tex1={tex1} tex2={tex2} transparent={true} colorSpace={THREE.SRGBColorSpace} />
           </mesh>
         </group>
