ff0f688cd56e6ede29af2a680e9787cb3f478cb4
Ahlecss <alex.sejourne1@gmail.com>
2024-07-09 21:51:08 +0200

    gyro

--- src/Gyro.js
+++ src/Gyro.js
@@ -14,10 +14,11 @@ export const Gyro = (isActive) => {
         const alpha = (e.alpha * Math.PI) / 180;
         // const beta = (e.beta * Math.PI) / 180;
         // const gamma = (e.gamma * Math.PI) / 180;
-        const diff = alpha - previous
-        console.log(diff)
-        if (diff > 0.002 || diff < - 0.002) setQuat(alpha)
-        previous = alpha
+        // const diff = alpha - previous
+        setQuat(alpha)
+        // console.log(diff)
+        // if (diff > 0.002 || diff < - 0.002) setQuat(alpha)
+        // previous = alpha
     }
 
     const setQuat = (alpha) => {
