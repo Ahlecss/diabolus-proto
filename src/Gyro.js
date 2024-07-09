9743be31bfa2c4fbb22b6ebb8ae59b660428e0df
Ahlecss <alex.sejourne1@gmail.com>
2024-07-09 20:47:02 +0200

    gyro

--- src/Gyro.js
+++ src/Gyro.js
@@ -7,16 +7,17 @@ export const Gyro = () => {
 
     const camera = useThree((state) => state.camera)
     let test = 0
+    let lla = 0
     let previous = 0;
 
     const deviceMotion = (e) => {
         console.log(e)
-        // test = +(e.alpha * 3,14159 / 180).toFixed(2)
-        test = (e.alpha * Math.PI) / 180
+        test = +(e.alpha).toFixed(2)
+        if(test !== test) lla = (e.alpha * Math.PI) / 180
     }
 
     useFrame((state, dt) => {
-        easing.damp3(state.camera.rotation, [0, test, 0], 0.1, dt)
+        easing.damp3(state.camera.rotation, [0, lla, 0], 0.1, dt)
         console.log(state.camera.rotation.y)
     })
 
