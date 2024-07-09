2ae7af9a4db4affbf2b92d1e93ee8e591e84cc81
Ahlecss <alex.sejourne1@gmail.com>
2024-07-09 22:10:01 +0200

    gyro

--- src/Frames.js
+++ src/Frames.js
@@ -165,7 +165,7 @@ export const Frames = memo(({ vitraux, q = new Quaternion(), p = new Vector3() }
     // if (hovered != 1) exposure = lerp(exposure, 0.2, 0.1)
     // else exposure = lerp(exposure, 0, 0.1)
     // easing.damp3(state.camera.position, p, circ.inOut(0.8), dt)
-    if (isMobile && clicked.current == null) {
+    if (isMobile && clicked.current !== null) {
       easing.dampQ(state.camera.quaternion, q, circ.inOut(0.6), dt)
     } else if (!isMobile && clicked.current !== null) {
       easing.dampQ(state.camera.quaternion, q, circ.inOut(0.6), dt)
