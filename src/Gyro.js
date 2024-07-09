ad3d3317578454f873e9ddd563ce6e6c5281a98a
Ahlecss <alex.sejourne1@gmail.com>
2024-07-09 19:39:53 +0200

    gyro

--- src/Gyro.js
+++ src/Gyro.js
@@ -4,21 +4,15 @@ import { useEffect } from "react"
 export const Gyro = () => {
 
     const camera = useThree((state) => state.camera)
-    let test = 0;
 
     const deviceMotion = (e) => {
         console.log(e)
-        console.log(`${e.acceleration.y} m/s2`);
-
-        
-        test += e.acceleration.y
-        console.log(test)
-        camera.rotation.y = test
+        camera.rotation.y = e.alpha
     }
 
 
     useEffect(() => {
-        window.addEventListener('devicemotion', deviceMotion)
-        return () => window.removeEventListener('devicemotion', deviceMotion)
+        window.addEventListener('deviceorientation', deviceMotion)
+        return () => window.removeEventListener('deviceorientation', deviceMotion)
     }, [])
 }
