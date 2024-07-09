fb19c2f1a2f7b9cd1e87c16bd1f6da01e659171f
Ahlecss <alex.sejourne1@gmail.com>
2024-07-09 19:23:49 +0200

    gyro

--- src/Gyro.js
+++ src/Gyro.js
@@ -9,7 +9,7 @@ export const Gyro = () => {
         console.log(e)
         console.log(`${e.acceleration.y} m/s2`);
 
-        camera.rotation.y = e.rotationRate.gamma
+        camera.rotation.y = e.rotationRate.gamma / 360
     }
 
 
