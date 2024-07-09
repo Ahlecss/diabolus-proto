a815f14aeb0c7f0bd60cd138b67a68e062d8ae30
Ahlecss <alex.sejourne1@gmail.com>
2024-07-09 20:54:05 +0200

    gyro

--- src/Gyro.js
+++ src/Gyro.js
@@ -2,6 +2,7 @@ import { useFrame, useThree } from "@react-three/fiber"
 import { easing } from "maath"
 import { cubic } from "maath/easing"
 import { useEffect } from "react"
+import { Euler, Quaternion } from "three"
 
 export const Gyro = () => {
 
@@ -11,14 +12,24 @@ export const Gyro = () => {
     let previous = 0;
 
     const deviceMotion = (e) => {
-        test = +(e.alpha).toFixed(3)
-        console.log(test)
-        if(test != test) lla = (test * Math.PI) / 180
+        // test = +(e.alpha).toFixed(3)
+        // console.log(test)
+        // if(test != test) lla = (test * Math.PI) / 180
+
+
+        const alpha = (e.alpha * Math.PI) / 180;
+        const beta = (e.beta * Math.PI) / 180;
+        const gamma = (e.gamma * Math.PI) / 180;
+
+        var euler = new Euler(beta, gamma, alpha, "YXZ"); // ' is the order of rotations
+        var quaternion = new Quaternion();
+        quaternion.setFromEuler(euler);
+        camera.setRotationFromQuaternion(quaternion);
     }
 
     useFrame((state, dt) => {
-        easing.damp3(state.camera.rotation, [0, lla, 0], 0.1, dt)
-        console.log(state.camera.rotation.y)
+        // easing.damp3(state.camera.rotation, [0, lla, 0], 0.1, dt)
+        // console.log(state.camera.rotation.y)
     })
 
 
