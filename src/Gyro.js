c5584b87f161a1ef1941b0f3f721f6ec286dc14e
Ahlecss <alex.sejourne1@gmail.com>
2024-07-09 21:40:59 +0200

    gyro

--- src/Gyro.js
+++ src/Gyro.js
@@ -7,27 +7,24 @@ import { Euler, Quaternion } from "three"
 export const Gyro = (isActive) => {
 
     const camera = useThree((state) => state.camera)
-    // let test = 0
-    // let lla = 0
-    // let previous = 0;
+    const previous = 0;
 
     const deviceMotion = (e) => {
 
         const alpha = (e.alpha * Math.PI) / 180;
-        const beta = (e.beta * Math.PI) / 180;
-        const gamma = (e.gamma * Math.PI) / 180;
+        // const beta = (e.beta * Math.PI) / 180;
+        // const gamma = (e.gamma * Math.PI) / 180;
+        const diff = alpha - previous
+        if (diff > 0.2 && diff < - 0.2) setQuat(alpha)
+        previous = alpha
+    }
 
+    const setQuat = (alpha) => {
         var euler = new Euler(0, alpha, 0, "YXZ"); // ' is the order of rotations
         var quaternion = new Quaternion();
         quaternion.setFromEuler(euler);
         camera.setRotationFromQuaternion(quaternion);
     }
-
-    // useFrame((state, dt) => {
-    //     // easing.damp3(state.camera.rotation, [0, lla, 0], 0.1, dt)
-    //     // console.log(state.camera.rotation.y)
-    // })
-
 
     useEffect(() => {
         if (isActive) window.addEventListener('deviceorientation', deviceMotion)
